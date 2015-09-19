0ff5bbf1eed11a01cacf8e45c4f83cea24eb0d69
emirpekdemir <aykutpekdemir@gmail.com>
2015-09-19 16:53:38 -0500

    updates on maps etc

--- dataviz.js
+++ dataviz.js
@@ -399,10 +399,10 @@ $(function () {
             type: 'spline'
         },
         title: {
-            text: 'A Chart'
+            text: 'Graffiti Cleanup'
         },
         xAxis: {
-            categories: ['A', 'B', 'C', 'D', 'E'],
+            categories: creation_dates,
         },
         yAxis: {
             min: 0,
@@ -415,7 +415,7 @@ $(function () {
         },
         series: [{
             name: 'foo',
-            data: [1, 2, 3, 2, 1]
+            data: count_by_date
         }]
     });
 
@@ -446,6 +446,10 @@ $(function () {
 
 
     var map = L.map('map').setView([41.88, -87.65], 12);
-    var layer = new L.StamenTileLayer("toner");
+    var layer = new L.StamenTileLayer("toner"); map.addLayer(layer);
+    var locations = graffiti_data['cement_graffiti_latlong']
+    for (var i=0; i<locations.length; i++){
+      marker = new L.circleMarker(locations[i], {color:"#D63821"}).setRadius(2).addTo(map);
+    }
 
 });
